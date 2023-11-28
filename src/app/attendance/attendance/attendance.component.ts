ffd510ff45c557ff523ec893e2984c5ebf57399a
Shemeeba <shemeebapunathil@gmail.com>
2023-11-28 12:45:33 -0500

    Showing the updated list

--- src/app/attendance/attendance/attendance.component.ts
+++ src/app/attendance/attendance/attendance.component.ts
@@ -67,7 +67,7 @@ export class AttendanceComponent implements OnInit {
         
   }
 
-  private getAttendanceList() {
+  getAttendanceList() {
     this.visibility = true;
    
     
@@ -186,16 +186,18 @@ export class AttendanceComponent implements OnInit {
                 });
               });
             })
+            this.getAttendanceList();
             this.messageService.add({
               severity: 'success',
               summary: 'Successful',
               detail: 'new attendances created',
               life: 3000,
             });
-            this.getAttendanceList();
-          }
+            
+          }        
       }}
     this.attendanceDialogue = false;
+    this.getAttendanceList();
   }}
   //delete
   deleteAttendance(attendance: Attendance) {
@@ -249,27 +251,20 @@ export class AttendanceComponent implements OnInit {
       accept: () => {
         this.attendances = this.attendances.filter(
           (val) => this.selectedAttendances.includes(val)
-        );
-        
+        );        
         this.attendances.forEach((value)=> (
-          this.attendanceService.delete(value).subscribe(response => {      
-          })
-        )), err => {
-          this.messageService.add({
-            severity: 'failure',
-            summary: 'Failed',
-            detail: 'Attendance deletion failed',
-            life: 3000,
-          });   
+          this.attendanceService.delete(value).subscribe(response => {   
+          })   
+        )) 
+        this.getAttendanceList();  
         this.messageService.add({
           severity: 'success',
           summary: 'Successful',
           detail: 'Selected attendances deleted',
           life: 3000,
         });
-      }
-    }
-   })
-
-  }
+        this.getAttendanceList();
+      }      
+    })
+   }
 }
