1f91be1d1830ba64906d8425f396ad19c37ab8f4
Bhagya sampath <bhagya.sampath13@gmail.com>
2024-01-12 17:53:07 -0500

    LMS-212-change-UID-emailId-change-roleId-update

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ConfirmationService, MessageService, SelectItem } from 'primeng/api';
-import { forkJoin } from 'rxjs';
 import { Batch } from 'src/app/batch/batch';
 import { BatchService } from 'src/app/batch/batch.service';
 import { Program } from 'src/app/program/program';
 import { ProgramService } from 'src/app/program/program.service';
 import { User } from '../user';
 import { UserService } from '../user.service';
-
 
 @Component({
   selector: 'app-user',
@@ -17,8 +15,8 @@ import { UserService } from '../user.service';
 })
 export class UserComponent implements OnInit {
   
-
   users: User[];
+  usersWithRolesList: User[];
   user:User;
   visibility: boolean = false;
   userSize: number;
@@ -30,8 +28,10 @@ export class UserComponent implements OnInit {
   userRoleMaps:string[]=['R01','R02','R03'];
   userRoleDropdown: SelectItem[];
   roleStatus:string[]=['Active','Inactive'];
+  roleStatusDropdown: SelectItem[]; //Jo
   userVisaStatus: string[] = ['Not-Specified', 'NA', 'GC-EAD', 'H4-EAD', 'H4', 'H1B', 
   'Canada-EAD', 'Indian-Citizen', 'US-Citizen', 'Canada-Citizen'];
+  userVisaStatusOptions: SelectItem[]; //Jo
   userVisaStatusControl: FormControl;
   visaStatusValue: string;
   userRoleStatusControl: FormControl;
@@ -48,6 +48,7 @@ export class UserComponent implements OnInit {
  userRoleEdit:boolean=false;
  progData1:any;
  userID:any;
+ userObject :boolean;
  
  constructor(private userService: UserService,
     private fb: FormBuilder,
@@ -59,9 +60,16 @@ export class UserComponent implements OnInit {
 
   ngOnInit(): void {  
     this.getUserList();
+    this.userService.getUsers().subscribe(res => {
+    this.usersWithRolesList = res;
+    },
+    (error) => {
+      console.log(error);
+    });
+    
     this.userVisaStatusControl = new FormControl();
     this.userVisaStatusControl.valueChanges.subscribe((val)=>{
-      console.log(val);
+     
       this.visaStatusValue=val;
     });
     this.userRoleStatusControl = new FormControl();
@@ -81,6 +89,8 @@ export class UserComponent implements OnInit {
       this.userList=list;
     });
     this.userRoleDropdown = this.userRoleMaps.map(role => ({ label: role, value: role }));
+    this.userVisaStatusOptions=this.userVisaStatus.map(vstatus => ({ label: vstatus, value: vstatus }));
+    this.roleStatusDropdown = this.roleStatus.map(rStatus => ({ label: rStatus, value: rStatus }));
   }
   
 updateFilteredBatchNames(){
@@ -109,8 +119,9 @@ updateFilteredBatchNames(){
   }
 
   hideDialog() {
+    this.user={};
     this.userDialogue = false;
-    this.viewUserDialogue=false;
+   // this.viewUserDialogue=false;
     this.assignProgBatchDialogue=false;
     this.submitted=false;
   }
@@ -126,7 +137,8 @@ this.userRoleEdit=false;
     this.user = {};
     this.submitted = false;
     this.userDialogue = true;
-    this.userForm.reset();
+    
+   this.userForm.reset();
   }
 
     userForm = this.fb.group({
@@ -138,23 +150,25 @@ this.userRoleEdit=false;
       userLastName: ['', Validators.required],
       userLinkedinUrl: ['', Validators.required],
       userLocation: ['', Validators.required],
-      userMiddleName: [''],
+      userMiddleName: ['',Validators.required],
       userPhoneNumber: ['', Validators.required],
       userTimeZone: ['', Validators.required],
       userVisaStatus: ['', Validators.required],
+      roleId : [''],
+      userRoleStatus: [''],
       userLogin: this.fb.group({
-        loginStatus: ['Active', Validators.required],
+        loginStatus: ['Active'],
         password: [''],
         userLoginEmail: ['', [Validators.required, Validators.email]],
       }),
       userRoleMaps: this.fb.array([
         this.fb.group({
-          roleId: ['',Validators.required],
-          userRoleStatus: ['',Validators.required],
+          roleId: [''],
+          userRoleStatus: [''],
         })
       ]),
     });
-    
+
   assignProgBatchForm = this.fb.group({
     programName: ['', Validators.required],
     batchName: ['', Validators.required],
@@ -164,7 +178,7 @@ this.userRoleEdit=false;
   });
  
   hasUnitNumber = false;
-
+ /*** 
   states = [
     { name: 'Alabama', abbreviation: 'AL' },
     { name: 'Alaska', abbreviation: 'AK' },
@@ -226,17 +240,16 @@ this.userRoleEdit=false;
     { name: 'Wisconsin', abbreviation: 'WI' },
     { name: 'Wyoming', abbreviation: 'WY' }
   ];
+  **/
 
-
-
-  
-  editUser(user: User) {
+editUser(user: User) {
     
     const userEmailAddress:string=user.userLoginEmail;
     this.userForm.patchValue(user);
     this.userForm.get('userLogin.userLoginEmail').patchValue(userEmailAddress);
     this.userDialogue = true;
     this.user={...user};
+    const targetUserId=user.userId;
     const getAllUsers$ = this.userService.getAllUsers();
     const getUsers$ = this.userService.getUsers();
     getAllUsers$.subscribe(
@@ -245,19 +258,36 @@ this.userRoleEdit=false;
     );
     
     getUsers$.subscribe(
-      userRoleMap1 => console.log('userRoleMap1:', userRoleMap1),
-      error => console.error('Error fetching userRoleMap1', error)
+      userRoleMap1 => {
+        console.log('userRoleMap1:', userRoleMap1);
+        const userMapWithRoles :any=userRoleMap1;
+        const testUser=userMapWithRoles.find(userMap => userMap.user?.userId == targetUserId);
+        console.log("testUser="+testUser);
+        const userRoleStatus1 = testUser.userRoleStatus;
+            const userRoleId1 = testUser.role.roleId;
+            this.userForm.get('userRoleStatus').setValue(userRoleStatus1);
+            this.userForm.get('roleId').setValue(userRoleId1);
+        },
+        error => console.error('Error fetching userRoleMap1', error)
     );
+
+   //const targetUserId=user.userId;
+    
+
+/**
     forkJoin([getAllUsers$, getUsers$]).subscribe(
       ([allUsers, userRoleMap1]) => {
         this.users = allUsers;
         allUsers.forEach(allUser => {
-          const correspondingUser = userRoleMap1.find(userMap => userMap.user?.userId == allUser.userId);
+          const correspondingUser = userRoleMap1.find(userMap => userMap.user?.userId == targetUserId);
           if (correspondingUser) {
             const userRoleStatus = correspondingUser.userRoleStatus;
             const userRoleId = correspondingUser.role.roleId;
-            this.userForm.get('userRoleMaps').get('0.userRoleStatus').setValue(userRoleStatus);
-            this.userForm.get('userRoleMaps').get('0.roleId').setValue(userRoleId);
+            //this.userForm.get('userRoleMaps').get('0.userRoleStatus').setValue(userRoleStatus);
+            //this.userForm.get('userRoleMaps').get('0.roleId').setValue(userRoleId);
+           this.userForm.get('userRoleStatus').setValue(userRoleStatus);
+          
+           this.userForm.get('roleId').setValue(userRoleId);
           this.user = { ...user, userId: user.userId };
 
           }
@@ -271,12 +301,14 @@ this.userRoleEdit=false;
         this.visibility = false;
       }
     ); 
-   
+    */
 
   }
 
   visaStatusChanged(event: any) {
-    this.visaStatusValue = event.value;
+    console.log("event=="+event);
+    //this.visaStatusValue = event.value;
+    this.userVisaStatus=event.value;
   }
 
   userRoleStatusChanged(value: string) {
@@ -293,6 +325,7 @@ this.userRoleEdit=false;
     if (this.userForm.valid) {
       if (this.userForm.value.userId) {//Edit User
         const updatedUser = { ...this.userForm.value };
+        
         const userData = {
           userId: this.userForm.value.userId,
           userComments: this.userForm.value.userComments,
@@ -324,25 +357,43 @@ this.userRoleEdit=false;
         },
         (error) => {
           console.error('Error updating user', error);
-          alert('Error updating user details.');
+          console.log(error.message);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Failed',
+            detail: error.error.message,
+            life: 3000,
+            
+          });
+          //alert('Error updating user details.');
         }
         );
         this.userDialogue = false;
        this.user = {};
       }
        else { //may be new User
-        this.userSize = this.userSize + 1;
-        this.user.userId = this.userSize.toString();
+       // this.userSize = this.userSize + 1;
+        //this.user.userId = this.userSize.toString();
         
     //  }
       this.userForm.get('userLogin.loginStatus').setValue('Active');
+    
+      const role=this.userForm.value.roleId;
+      const testRoleStatus =this.userForm.value.userRoleStatus;
+      this.userForm.get('userRoleMaps').get('0.roleId').setValue(role);
+      this.userForm.get('userRoleMaps').get('0.userRoleStatus').setValue(testRoleStatus);
         const userData = this.userForm.value;
         this.userForm.controls['userVisaStatus'].setValue(this.visaStatusValue);
-        const role=this.roleStatusValue;
-        this.userForm.get('userRoleMaps').get('0.userRoleStatus').setValue(role);
+       // const role=this.roleStatusValue;
+      //  this.userForm.get('userRoleMaps').get('0.userRoleStatus').setValue(role);
         if (this.userForm.value && this.userForm.value.userPhoneNumber) {
           this.userForm.value.userPhoneNumber = parseInt(this.userForm.value.userPhoneNumber, 11);
         }
+        
+        delete userData.roleId;
+        delete userData.userRoleStatus;
+        delete userData.userId;
+        
 
         this.userService.addUser(userData).subscribe({
           next:(res) => {
@@ -354,25 +405,28 @@ this.userRoleEdit=false;
               detail: 'User Added Successfully',
               life: 3000,
           });
+            this.getUserList();
             this.user={};
-            
           },
-         error:() =>
+         error:(error) =>
           {
-          
-            alert("Error adding user details.");
+            console.log("error=="+error.message);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Failed',
+              detail: error.error.message,
+              life: 2000,
+              
+            });
+           // alert("Error adding user details.");
           }
         })
-
-    
       this.userDialogue = false;
       this.user= {};
-
-      
     }
   }
 }
-  
+    
   deleteSelectedUsers() {
     this.confirmationService.confirm({
         message: 'Are you sure you want to delete the selected Users?',
@@ -424,7 +478,7 @@ this.userRoleEdit=false;
       
       const userPBData = {
         programId:pid,
-userId:this.userID,
+        userId:this.userID,
         roleId:this.assignProgBatchForm.value.roleId,
                 userRoleProgramBatches:[
           {
@@ -445,6 +499,16 @@ userId:this.userID,
       
       }, (error)=> {
             this.assignProgBatchDialogue=false;
+            this.messageService.add({
+            severity: 'error',
+            summary: 'Failed',
+            detail: error.error.message,
+            life: 2000,
+        });
+      });
+
+        /**
+            this.assignProgBatchDialogue=false;
             console.error('Other error:', error.status, error.statusText);
             if(error.status === 200){
               console.log("true");
@@ -463,7 +527,8 @@ userId:this.userID,
               life: 2000,
             });
           }
-      });
+           */
+     // });
     } else {
       console.log("Invalid form");
     }
@@ -477,14 +542,15 @@ userId:this.userID,
         userRoleMap1 => console.log('userRoleMap1:', userRoleMap1),
         error => console.error('Error fetching userRoleMap1', error)
       );
-      forkJoin([getUsers$]).subscribe(
-        ([userRoleMap1]) => {
-            const correspondingUser = userRoleMap1.find(userMap => userMap.user?.userId == this.userID);
-            if (correspondingUser) {
-              const userRoleId = correspondingUser.role.roleId;
+      getUsers$.subscribe(
+        userRoleMap1 => {
+          console.log('userRoleMap1:', userRoleMap1);
+          const userMapWithRoles :any=userRoleMap1;
+          const testUser=userMapWithRoles.find(userMap => userMap.user?.userId == this.userID);
+              const userRoleId = testUser.role.roleId;
               this.assignProgBatchForm.value.roleId=userRoleId;
               this.userRoleEdit=true;
-               }
+      
           });
       }
       
